6a5af62a1358726d612322f99b3c8e50bc18a59c
Slakky <c.novella.rausell@gmail.com>
2019-09-12 17:27:22 +0200

    finally it will work

---
+++ scripts/10_LinLoc.R
@@ -0,0 +1,3 @@
+library(bugwas)
+lin_loc(gen='/home/claudio/Comp_Genetics/result_true/bugwas_genotypes_2_NV.tsv', pheno='/home/claudio/Comp_Genetics/result_true/bugwas_phenotypes_1.tsv', phylo='/home/claudio/Comp_Genetics/result_true/nonull_filtered_tree.nwk', prefix='ecoref', output.dir='/home/claudio/Comp_Genetics/', gem.path='/home/claudio/Comp_Genetics/bugwas/gemma/gemma.0.93b')
+
