b53d86df196c2451533995cd231631512f878854
Slakky <c.novella.rausell@gmail.com>
2019-09-16 18:01:54 +0200

    added new defninitive filtering notebook

--- scripts/10_LinLoc.R
+++ scripts/10_LinLoc.R
@@ -1,3 +1,2 @@
 library(bugwas)
-lin_loc(gen='/home/claudio/Comp_Genetics/result_true/bugwas_genotypes_2_NV.tsv', pheno='/home/claudio/Comp_Genetics/result_true/bugwas_phenotypes_1.tsv', phylo='/home/claudio/Comp_Genetics/result_true/nonull_filtered_tree.nwk', prefix='ecoref', output.dir='/home/claudio/Comp_Genetics/', gem.path='/home/claudio/Comp_Genetics/bugwas/gemma/gemma.0.93b')
-
+lin_loc(gen='/home/claudio/Comp_Genetics/result_true/bugwas_genotypes_2_NV.tsv', pheno='/home/claudio/Comp_Genetics/result_true/bugwas_phenotypes_4.tsv', phylo='/home/claudio/Comp_Genetics/result_true/nonull_filtered_tree.nwk', prefix='ecoref', gem.path='/home/claudio/Comp_Genetics/bugwas/gemma/gemma.0.93b', output.dir='.')
