04b5e924aa96da192aea677fd561b9664578c1ab
Slakky <c.novella.rausell@gmail.com>
2019-11-06 15:51:53 +0100

    brewer bug

--- scripts/final/06_Heatmap.R
+++ scripts/final/06_Heatmap.R
@@ -1,7 +1,7 @@
 library(gplots)
 library(RColorBrewer)
 df <- read.csv(file = "/home/claudio/nas/output/heatmap_dataframe.tsv", sep = "\t", header = TRUE)
-myCol <- brewer.pal(n = 8, name = "Dark2")
+myCol <- brewer.pal(n = 3, name = "Dark2")
 myBreaks <- c(0, 7, 5, 1) # pvalues are log transformed
 tiff('/home/claudio/nas/heatmap.tiff', width = 1000, height = 1000, compression = "lzw", res = 300)
 heatmap.2(as.matrix(df[,-1]), dendrogram = "row", Colv = FALSE, tracecol = NA, breaks = myBreaks, col = myCol) # [,-1] because the first column is character and heatmap.2 requires numeric matrix
