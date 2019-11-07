e2ec9bf2de78cbc8dda98702368f31c6ebfea0c0
Slakky <c.novella.rausell@gmail.com>
2019-11-07 11:14:24 +0100

    modified heatmap

--- scripts/final/06_Heatmap.R
+++ scripts/final/06_Heatmap.R
@@ -1,7 +1,7 @@
 library(gplots)
 library(RColorBrewer)
 df <- read.csv(file = "/home/claudio/nas/output/heatmap_dataframe.tsv", sep = "\t", header = TRUE)
-myCol <- brewer.pal(n = 3, name = "Dark2")
-myBreaks <- c(0, 7, 5, 1) # pvalues are log transformed
+myCol <- c("#e5f5f9","#99d8c9","#2ca25f")
+myBreaks <- c(1, 5, 7, 20) # pvalues are log transformed
 tiff('/home/claudio/nas/heatmap.tiff', width = 1000, height = 1000, compression = "lzw", res = 300)
-heatmap.2(as.matrix(df[,-1]), dendrogram = "row", Colv = FALSE, tracecol = NA, breaks = myBreaks, col = myCol) # [,-1] because the first column is character and heatmap.2 requires numeric matrix
+heatmap.2(as.matrix(df[,-1]), dendrogram = "row", Colv = FALSE, Rowv=FALSE, density.info = "none", trace = "none", breaks = myBreaks, col = myCol, symkey = FALSE) # [,-1] because the first column is character and heatmap.2 requires numeric matrix
