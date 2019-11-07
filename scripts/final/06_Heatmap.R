9cd854673efb2f0e54a6581ec84cd8e877591289
Slakky <c.novella.rausell@gmail.com>
2019-11-07 18:31:22 +0100

    final heatmap

--- scripts/final/06_Heatmap.R
+++ scripts/final/06_Heatmap.R
@@ -1,7 +1,7 @@
 library(gplots)
 library(RColorBrewer)
 df <- read.csv(file = "/home/claudio/nas/output/heatmap_dataframe.tsv", sep = "\t", header = TRUE)
-myCol <- brewer.pal(4, "Set3")[3:4]
+myCol <- c("white", "black")
 myBreaks <- c(1, 7, 10)
 tiff('/home/claudio/nas/heatmap.tiff', width = 3200, height = 3200, units = "px", res = 800, compression = "lzw")
 heatmap.2(as.matrix(df[,-1]), dendrogram = "row",  
@@ -12,7 +12,10 @@ heatmap.2(as.matrix(df[,-1]), dendrogram = "row",
           lhei = c(1.5,9),
           keysize = 0.3,
           key.par = list(cex=0.3),
+          tracecol="orange",
           symkey = FALSE,
+          key.title = paste(""),
+          key.ylab = paste(""),
           key.xlab = paste("-log10(P-value)"),
           labRow = FALSE, labCol = FALSE) # [,-1] because the first column is character 
 dev.off()
